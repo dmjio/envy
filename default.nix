8bf04d32385054900380bf627731f1bdfe6816ea
David Johnson <djohnson.m@gmail.com>
2018-11-14 17:31:39 -0500

    Bump build to 8.4.4

--- default.nix
+++ default.nix
@@ -1,18 +1,18 @@
-{ compiler ? "ghc822" }:
+{ compiler ? "ghc844" }:
 let
-   config = {
-     packageOverrides = pkgs: with pkgs.haskell.lib; {
-       haskell = pkgs.haskell // {
-         packages = pkgs.haskell.packages // {
-	   ${compiler} = pkgs.haskell.packages.${compiler}.override {
-	     overrides = self: super: {
-	       envy = buildStrictly (self.callPackage ./envy.nix {});
-             };
-           };
-         };
-       };
+  config = {
+    packageOverrides = pkgs: with pkgs.haskell.lib; {
+      haskell = pkgs.haskell // {
+        packages = pkgs.haskell.packages // {
+          ${compiler} = pkgs.haskell.packages.${compiler}.override {
+            overrides = self: super: {
+              envy = buildStrictly (self.callPackage ./envy.nix {});
+            };
+          };
+        };
+      };
     };
- };
+  };
 in
   with (import <nixpkgs> { inherit config; }).haskell.packages.${compiler};
   envy
